7252017a1ca6e05898ab97e62ec0daf5ea6e4d3e
andimuaffivatulfiqra <127370910+andimuaffivatulfiqra@users.noreply.github.com>
2023-03-11 20:24:10 +0700

    Create Asign.java

---
+++ Asign.java
@@ -0,0 +1,18 @@
+// 13020210029,Andi Muaffivatul Fiqra, 
+//Rabu/08-03-2023,17:00//
+
+public class Asign {
+    /**
+    * @param args
+    */
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus */
+        int i;
+        /* Program */
+
+        System.out.print ("hello\n"); i = 5;
+        System.out.println ("Ini nilai i : " + i); i = 5;
+    }
+}
